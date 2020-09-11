0277b6a8c25cc0940b113d1840059af5940832aa
jlpoweregg <jlpoweregg@gmail.com>
2020-09-11 23:38:03 +0000

    more inputs and print

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 food = input("pick a food")
-foood = input("pick another food")
+food2 = input("pick another food")
 place = input("pick a place")
-superpower = input("")
+superpower = input("enter a superpower")
+season = input("pick a season of the year")
+place2 = input("pick another place")
+
+print(f"If I eat {food} with {food2} on top during {season} in {place}, then I can get th superpower of {superpower}!!!            A few months later...       Wait a second, if you eat {food2} with {food} on top under {place2}, you can teleport to {food}{food2} island! Wow!")
