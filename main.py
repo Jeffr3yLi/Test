3ff14ea555b7fc3867cffbe06593e8d7abf16b16
jlpoweregg <jlpoweregg@gmail.com>
2020-09-11 23:41:08 +0000

    more variables

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ place = input("pick a place")
 superpower = input("enter a superpower")
 season = input("pick a season of the year")
 place2 = input("pick another place")
+animal = input("pick a animal")
 
-print(f"If I eat {food} with {food2} on top during {season} in {place}, then I can get th superpower of {superpower}!!!            A few months later...       Wait a second, if you eat {food2} with {food} on top under {place2}, you can teleport to {food}{food2} island! Wow!")
+print(f"If I eat {food} with {food2} on top during {season} in {place}, then I can get th superpower of {superpower}!!!            A few months later...       Wait a second, if a {animal} eats {food2} with {food} on top under {place2}, you can teleport to {food}{food2} island! Wow!")
